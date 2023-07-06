c8142cddd3ecc8e9864650b9a4ababd654fe0e5a
Diego Romero <deromero707@gmail.com>
2023-07-06 11:08:44 -0400

    Finish V1

---
+++ src/data/skillset.js
@@ -0,0 +1,46 @@
+export const skillset = [
+  {
+    title: 'Next JS',
+    skillIcon: '../skills/next-js.svg'
+  },
+  {
+    title: 'React JS',
+    skillIcon: '../skills/react.svg'
+  },
+  {
+    title: 'Javascript',
+    skillIcon: '../skills/javascript.svg'
+  },
+  {
+    title: 'Astro Build',
+    skillIcon: '../skills/astro.svg'
+  },
+  {
+    title: 'HTML',
+    skillIcon: '../skills/html.svg'
+  },
+  {
+    title: 'CSS',
+    skillIcon: '../skills/css-alt.svg'
+  },
+  {
+    title: 'Tailwind',
+    skillIcon: '../skills/tailwindcss.svg'
+  },
+  {
+    title: 'Typescript',
+    skillIcon: '../skills/typescript.svg'
+  },
+  {
+    title: 'Github',
+    skillIcon: '../skills/github.svg'
+  },
+  // {
+  //   title: 'NPM',
+  //   skillIcon: '../skills/npm.svg'
+  // },
+  {
+    title: 'Webpack',
+    skillIcon: '../skills/webpack.svg'
+  },
+]
