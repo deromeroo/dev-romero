23ada1464e9ac14c146a936b15cedc74ae4bb030
Diego Romero <deromero707@gmail.com>
2023-08-16 18:17:21 -0400

    UPDATE DESING / FUNCTIONAL

--- src/data/projects.js
+++ src/data/projects.js
@@ -2,25 +2,25 @@ export const projects = [
   {
     title: 'Github User',
     description: 'Search Github users, obtaining information about them (repositories, followers). Developed using React.js, Material UI and Github API',
-    image: '/captures/github-user.webp',
+    image: '/captures/github.webp',
     link: 'https://deromeroo.github.io/Github-User-Search/'
   },
   {
     title: 'Platzi Conf Merch',
-    description: 'Ecommerce website for the sale of products from the Platzi Conf. Created using React.js, implementing Paypal as a payment method, and Geolocation (Leaflet / Geoapify).',
-    image: '/captures/platziConf.webp',
+    description: 'Ecommerce website for the sale of products from the Platzi Conf. Developed using React.js, implementing Paypal as a payment method, and Geolocation (Leaflet / Geoapify).',
+    image: '/captures/conf.webp',
     link: 'https://deromeroo.github.io/Platzi-Conf-Merch/'
   },
   {
-    title: 'Github User',
+    title: 'Yard Sale',
     description: 'Ecommerce(cart). Developed using React.js and consuming an API(products).',
-    image: '/captures/yard-screen.webp',
+    image: '/captures/yard.webp',
     link: 'https://deromeroo.github.io/Yard-Shop/'
   },
   {
     title: 'Todo App',
     description: 'Todo Web Application to take note of the tasks you have to do. Create using React.js and implementing localstorage.',
-    image: '/captures/TodoApp.webp',
+    image: '/captures/todo.webp',
     link: 'https://deromeroo.github.io/TodoApp/'
   },
 ]
